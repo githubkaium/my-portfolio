c153549c53815a3e127e9edd4eb71f8a82a29429
githubkaium <mailtokaium@gmail.com>
2021-12-07 18:43:52 +0600

    topbar update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import "./topbar.scss"
-import { Person, Mail } from "@material-ui/icons"
+import { Mail, GitHub, LinkedIn, Call } from "@material-ui/icons"
+
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
@@ -8,12 +9,22 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
                 <div className="left">
                     <a href="#intro" className="logo">My Portfolio</a>
                     <div className="itemContainer">
-                        <Person className="icon" />
-                        <span>+880 1799 099 990</span>
+                        <a className="iconLink" href="https://www.linkedin.com/in/mohammad-al-kaium" target="_blank">
+                            <LinkedIn className="icon" /></a>
                     </div>
                     <div className="itemContainer">
-                        <Mail className="icon" />
-                        <span>mailtokaium@gmail.com</span>
+                        <a className="iconLink" href="https://github.com/githubkaium" target="_blank">
+                            <GitHub className="icon" /></a>
+                    </div>
+                    <div className="itemContainer">
+                        <a className="iconLink" href="#contact">
+                            <Mail className="icon" />
+                            <span>mailtokaium@gmail.com</span></a>
+                    </div>
+                    <div className="itemContainer">
+                    <a className="iconLink" href="#contact">
+                        <Call className="icon" />
+                        <span>+880 1799 099 990</span></a>
                     </div>
                 </div>
                 <div className="right">
